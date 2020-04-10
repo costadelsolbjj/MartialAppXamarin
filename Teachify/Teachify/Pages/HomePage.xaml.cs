3019190c5745e60f2026e892c21cc689795c5dfb
Doug bjj <costadelsolbjj@outlook.com>
2020-04-10 02:52:21 +0200

    Add project files.

---
+++ Teachify/Teachify/Pages/HomePage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MartialApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teachify.Models;
+using Teachify.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Teachify.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public ObservableCollection<Trainers> Instructors;
+        private bool First = true;
+        public HomePage()
+        {
+            InitializeComponent();
+            Instructors = new ObservableCollection<Trainers>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (First)
+            {
+                ApiService apiService = new ApiService();
+                var instructors = await apiService.GetInstructors();
+                foreach (var instructor in instructors)
+                {
+                    Instructors.Add(instructor);
+                }
+
+                LvInstructors.ItemsSource = Instructors;
+                BusyIndicator.IsRunning = false;
+            }
+
+            First = false;
+
+        }
+
+        private void LvInstructors_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedInstructor = e.SelectedItem as Trainers;
+            if (selectedInstructor != null)
+            {
+                Navigation.PushAsync(new InstructorProfilePage(selectedInstructor.TrainerId));
+            }
+
+            ((ListView) sender).SelectedItem = null;
+        }
+
+
+        private void TbSearch_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FindInstructorPage());
+        }
+    }
+}
---
+++ Teachify/Teachify/Pages/InstructorProfilePage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teachify.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Teachify.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InstructorProfilePage : ContentPage
+    {
+        private string number;
+        private string email;
+        public InstructorProfilePage(int id)
+        {
+            InitializeComponent();
+            GetInstructorProfile(id);
+        }
+
+        public async void GetInstructorProfile(int id)
+        {
+            ApiService apiService = new ApiService();
+            var instructor = await apiService.GetInstructor(id);
+            //ImgProfile.Source = instructor.FullLogoPath;
+            LblCity.Text = instructor.Name;
+            LblLanguage.Text = instructor.LastName;
+            LblNationality.Text = instructor.Email;
+            LblExperience.Text = instructor.Email;
+            LblSpecialization.Text = instructor.Email;
+            LblName.Text = instructor.Name;
+            LblOneLineDescription.Text = instructor.Tarifa;
+            LblHourlyRate.Text = instructor.Tarifa;
+            LblDescription.Text = instructor.Phone;
+            number = instructor.Phone;
+            email = instructor.Email;
+        }
+
+        private void BtnCall_OnClicked(object sender, EventArgs e)
+        {
+            PhoneDialer.Open(number);
+        }
+
+        private void BtnSms_OnClicked(object sender, EventArgs e)
+        {
+            var message = new SmsMessage("",number);
+            Sms.ComposeAsync(message);
+        }
+
+        private void BtnEmail_OnClicked(object sender, EventArgs e)
+        {
+            var message = new EmailMessage("","",email);
+            Email.ComposeAsync(message);
+        }
+    }
+}
